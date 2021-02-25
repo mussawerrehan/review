117c5a772ecc14219a78eb42bb05dadcf968bb53
Mussawer Alyasah <mussawer.alyassah@coeus-solutions.de>
2021-02-26 04:43:43 +0500

    Add swagger

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     App\Coeus\TestBundle\CoeusTestBundle::class => ['all' => true],
     Trikoder\Bundle\OAuth2Bundle\TrikoderOAuth2Bundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
